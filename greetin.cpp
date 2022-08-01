b86cc56e98bd6d179c4f4632c03b7decba87726a
misarb <lahssane12@gmail.com>
2022-08-01 23:26:52 +0100

    update String inpStyling

--- greetin.cpp
+++ greetin.cpp
@@ -2,22 +2,25 @@
 
 
 #include <iostream>
+using namespace std;
 int main()
 {
 
 std::cout << "Please enter your name" << std::endl;
 std::string name ;
 std::cin>>name;
-const std::string greeting = "hello"+name+"!";
+const std::string greeting = "Hello "+name+"!";
+cout<<greeting.size();
 const std::string spaces(greeting.size(),' ');
 const std::string second = "* " +spaces+" *";
 const std::string first(second.size(),'*');
 std::cout <<std::endl;
 std::cout <<first<<std::endl;
 std::cout <<second<<std::endl;
-std::cout <<"*"<<greeting<<" *"<<std::endl;
-std::cout <<first<<std::endl;
+std::cout <<"* "<<greeting<<" *"<<std::endl;
 std::cout <<second<<std::endl;
+std::cout <<first<<std::endl;
+
 
 
 
