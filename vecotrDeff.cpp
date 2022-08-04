f0f57d1302077ef47e6bcfde44e76c5669d7f4bb
misarb <lahssane12@gmail.com>
2022-08-04 19:41:08 +0100

    update vec

--- vecotrDeff.cpp
+++ vecotrDeff.cpp
@@ -12,26 +12,24 @@ int main() {
     int count=0;
     double x;
     vector <double> homework;
-     typedef vector<double>::size_type vec_size;
-     vec_size mid,size;
+    typedef vector<double>::size_type vec_size;
+    vec_size mid,size;
 
    
     cout<<"Enter your name = ";
     cin>>name;
-    cout<<"Enter your midter, "
+    cout<<"Enter your midterm, "
     "and finTerm" <<endl;
     cin>>midter>>finterm;
     cout<<"ENTER YOUR HOMEWORK MARKS: "<<endl;
     while(cin>>x){
        
         homework.push_back(x);
-        
-     
-         size = homework.size();
+        size = homework.size();
         if(size==0) return 1;
     }
   
-    sort(homework.begin(),homework.end());
+    sort(homework.begin(),homework.end()); //srting the vector
     mid=size/2;
     double midian = size%2==0?(homework[mid]+homework[mid-1])/2:(homework[mid]);
     
